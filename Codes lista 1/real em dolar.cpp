21f1cca2663ffdf181d85bd0d8929085521458ba
Marília Gabriele <161947955+Marilia16@users.noreply.github.com>
2025-04-09 10:12:07 -0300

    Create real em dolar.cpp

---
+++ Codes lista 1/real em dolar.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () { 
+    float real, dolar, precoDodolar;
+    cout<<"\n\t\t adicione o valor em real:";
+    cin>> real;
+    cout<<"\t\t adicione o preco do dolar atual:";
+    cin>>precoDodolar;
+    dolar= precoDodolarreal;
+    cout<<"\t\t o preco em dolar e "<<dolar;
+}
